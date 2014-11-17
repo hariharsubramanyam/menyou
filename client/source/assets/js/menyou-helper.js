8ca19dbad7eaa4eec6d7a0216cef37d4974285bf
Ryan Chipman <rchipman@mit.edu>
2014-11-17 09:40:37 -0500

    fleshed out requests for helper and wrote skeleton for response handling

--- client/source/assets/js/menyou-helper.js
+++ client/source/assets/js/menyou-helper.js
@@ -1,26 +1,108 @@
 
-var Locu = {};
+var Menyou = {};
 
-Locu.getToken(username, password, callback) {
+Menyou.getToken(username, password, callback) {
   // POST /api/token
+  $.ajax({
+    url: '/api/token',
+    type: 'POST',
+    headers: {
+      'Accept': 'appliction/json; charset=utf-8',
+      'Content-Type': 'application/json; charset=utf-8'
+    },
+    data: JSON.stringify({
+      username: username,
+      password: password
+    })
+  });
 };
 
-Locu.register(username, password, callback) {
+Menyou.register(username, password, callback) {
   // POST /api/register
+  $.ajax({
+    url: '/api/register',
+    type: 'POST',
+    headers: {
+      'Accept': 'appliction/json; charset=utf-8',
+      'Content-Type': 'application/json; charset=utf-8'
+    },
+    data: JSON.stringify({
+      username: username,
+      password: password
+    })
+  });
 };
 
-Locu.validateToken(callback) {
+Menyou.validateToken(callback) {
   // GET /api/validate
+  $.ajax({
+    url: '/api/validate',
+    type: 'GET',
+    headers: {
+      'Accept': 'appliction/json; charset=utf-8',
+      'Content-Type': 'application/json; charset=utf-8',
+      'Authorization': 'Bearer ' + Menyou.token
+      // TODO: implement a cookie-based session store
+    }
+  });
 };
 
-Locu.getDishes(callback) {
+Menyou.getDishes(callback) {
   // GET /api/dishes/
+  $.ajax({
+    url: '/api/token',
+    type: 'POST',
+    headers: {
+      'Accept': 'appliction/json; charset=utf-8',
+      'Content-Type': 'application/json; charset=utf-8',
+      'Authorization': 'Bearer ' + Menyou.token
+    }
+  });
 };
 
-Locu.getTasteProfile(callback) {
+Menyou.getTasteProfile(callback) {
   // GET /api/taste/
+  $.ajax({
+    url: '/api/token',
+    type: 'POST',
+    headers: {
+      'Accept': 'appliction/json; charset=utf-8',
+      'Content-Type': 'application/json; charset=utf-8',
+      'Authorization': 'Bearer ' + Menyou.token
+    }
+  });
 };
 
-Locu.updateTesteProfile(add, remove, callback) {
+Menyou.updateTasteProfile(add, remove, callback) {
   // PUT /api/taste/
+  $.ajax({
+    url: '/api/token',
+    type: 'POST',
+    headers: {
+      'Accept': 'appliction/json; charset=utf-8',
+      'Content-Type': 'application/json; charset=utf-8',
+      'Authorization': 'Bearer ' + Menyou.token
+    },
+    data: JSON.stringify({
+      add: add,
+      remove: remove
+    }),
+    statusCode: {
+      200: function(data) {
+        console.log('OKAY');
+      },
+      304: function(jqxhr) {
+        console.log('NOT CHANGED');
+      },
+      401: function(jqxhr) {
+        console.log('NOT AUTHORIZED');
+      },
+      404: function(jqxhr) {
+        console.log('NOT FOUND');
+      },
+      500: function(jqxhr) {
+        console.log('INTERNAL SERVER ERROR')
+      }
+    }
+  });
 };
