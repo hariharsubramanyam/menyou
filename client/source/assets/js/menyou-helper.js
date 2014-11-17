49e97d6d04f89f497d2692289f854c5acb80549e
Ryan Chipman <rchipman@mit.edu>
2014-11-17 09:18:55 -0500

    added menyou helper skeleton

---
+++ client/source/assets/js/menyou-helper.js
@@ -0,0 +1,26 @@
+
+var Locu = {};
+
+Locu.getToken(username, password, callback) {
+  // POST /api/token
+};
+
+Locu.register(username, password, callback) {
+  // POST /api/register
+};
+
+Locu.validateToken(callback) {
+  // GET /api/validate
+};
+
+Locu.getDishes(callback) {
+  // GET /api/dishes/
+};
+
+Locu.getTasteProfile(callback) {
+  // GET /api/taste/
+};
+
+Locu.updateTesteProfile(add, remove, callback) {
+  // PUT /api/taste/
+};
