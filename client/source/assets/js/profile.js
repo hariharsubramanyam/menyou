6e5a2fc062806b154c3111eb46574fd5875e9736
Danielle Man <daniman0808@gmail.com>
2014-11-17 18:46:15 -0500

    profile page reads likes and dislikes from user db

--- client/source/assets/js/profile.js
+++ client/source/assets/js/profile.js
@@ -1,12 +1,17 @@
 (function() {
 
 $(document).on('click', '#profile', function(evt) {
-    var token = $.cookie(Menyou.COOKIE_NAME);
+    // var token = $.cookie(Menyou.COOKIE_NAME);
+    // var username = $.cookie(Menyou.USERNAME)
+    var token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiNTQ2YTMxMWE0OWNiNjM0ODMwYmEzZDhhIiwiZXhwaXJlcyI6MTQxNjMzNDQ0MzU0OH0.U8L7sTHkYiNjbLC5rMOzwYfvKXUmNTltvuQIHUnC7e0";
+    var username = "daniman";
+
     Menyou.APIHelper.getTasteProfile(token, function(tasteProfile) {
-        Menyou.UI.loadPage('profile', $.extend(
-            username: Menyou.USERNAME,
-            taste: tasteProfile.content
-        ));
+        Menyou.UI.loadPage('profile', {
+            username: username,
+            likes: tasteProfile.content.likes,
+            dislikes: tasteProfile.content.dislikes
+        });
     })
 });
 
