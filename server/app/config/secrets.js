137a6a37bd1a12b4d8d82fdf1c005e7dee527acc
Harihar Subramanyam <hahasubbu@hotmail.com>
2014-11-18 19:18:36 -0500

    Changed Locu API key to new account

--- server/app/config/secrets.js
+++ server/app/config/secrets.js
@@ -5,6 +5,6 @@
  * anybody to see. However, since the 6.170 TAs need to be able to run our project (and they don't
  * want to make a Locu account and get their own API key), we make this file visible on GitHub.
  */
-module.exports.LOCU_API_KEY = "f165c0e560d0700288c2f70cf6b26e0c2de0348f";
+module.exports.LOCU_API_KEY = "f045f67e6bc1ee73f771c642cb4e619f9774d12c";
 module.exports.SALT = 10;
 module.exports.TOKEN_SECRET = "this_is_a_secret";
