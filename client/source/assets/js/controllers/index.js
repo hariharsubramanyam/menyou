c295a0ebbb40b90a236a2480c68bacef8221147d
Harihar Subramanyam <hahasubbu@hotmail.com>
2014-11-18 19:04:29 -0500

    Handlebars ellipsize

--- client/source/assets/js/controllers/index.js
+++ client/source/assets/js/controllers/index.js
@@ -14,4 +14,19 @@
     Menyou.UI.render('index');
   });
 
+  /**
+   * Ellipsize text (i.e. if it is too long, cut it short and add "..." at the end).
+   *
+   * Handlebars helper for use on the index page.
+   */
+  Handlebars.registerHelper('ellipsize', function(options) {
+    var MAX_LENGTH = 300;
+    var str = options.fn(this);
+    if (str.length > MAX_LENGTH) {
+      return new Handlebars.SafeString(str.substring(1, MAX_LENGTH) + "...");
+    } else {
+      return new Handlebars.SafeString(str);
+    }
+  });
+
 })();
