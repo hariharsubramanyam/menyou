714b75e8e1aecba349f5afcc4df45e140cc98c0f
Harihar Subramanyam <hahasubbu@hotmail.com>
2014-11-21 09:16:36 -0500

    Changed API key so that we don't use the same key as the deployed version

--- server/app/config/secrets.js
+++ server/app/config/secrets.js
@@ -5,6 +5,6 @@
  * anybody to see. However, since the 6.170 TAs need to be able to run our project (and they don't
  * want to make a Locu account and get their own API key), we make this file visible on GitHub.
  */
-module.exports.LOCU_API_KEY = "f045f67e6bc1ee73f771c642cb4e619f9774d12c";
+module.exports.LOCU_API_KEY = "cdfb728fb1bb599f80c93cc098ac10b3a6e35049";
 module.exports.SALT = 10;
 module.exports.TOKEN_SECRET = "this_is_a_secret";
