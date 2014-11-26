aece153b9fe9f2980254cc7fab385ce352af49f6
Harihar Subramanyam <hahasubbu@hotmail.com>
2014-11-26 10:16:16 -0500

    Load mappings into main memory

--- server/app/controllers/mappings.js
+++ server/app/controllers/mappings.js
@@ -8,6 +8,13 @@ var router = express.Router();
 var resHelper = require('../helpers/response-helper.js');
 
 /**
+ * We'll load the mappings into main memory so that we don't need to go to the database each time
+ * a request is made. This is reasonable because the number of mappings is (and always will be)
+ * small - after all, we need to write them by hand.
+ */
+var mappings = null;
+
+/**
  * Get a list of mappings.
  *
  * Request: 
@@ -31,25 +38,31 @@ var resHelper = require('../helpers/response-helper.js');
  * On error, the "success" field will be false.
  */
 router.get("/", function(req, res) {
-  // Note there is no harm in getting all the mappings, because we (menyou team) are the ones
-  // who create them, so we know exactly how many there are. Furthermore, the number of mappings
-  // will be small, so there is no risk in searching for them all and returning them to the user.
-  req.model.Mapping.find({}, function(err, mapping_objects) {
-    // Remove extraneous fields like the version number and id
-    var mappings = [];
-    mapping_objects.forEach(function(mapping_object) {
-      mappings.push({
-        "source": mapping_object.source,
-        "targets": mapping_object.targets
+  // If the mappings are in main memory, return them.
+  // Otherwise, fetch them from the database.
+  if (mappings !== null) {
+    resHelper.success(res, "Mappings", mappings);
+  } else {
+    // Note there is no harm in getting all the mappings, because we (menyou team) are the ones
+    // who create them, so we know exactly how many there are. Furthermore, the number of mappings
+    // will be small, so there is no risk in searching for them all and returning them to the user.
+    req.model.Mapping.find({}, function(err, mapping_objects) {
+      // Remove extraneous fields like the version number and id
+      mappings = [];
+      mapping_objects.forEach(function(mapping_object) {
+        mappings.push({
+          "source": mapping_object.source,
+          "targets": mapping_object.targets
+        });
       });
-    });
 
-    if (err) {
-      resHelper.error(res, err);
-    } else {
-      resHelper.success(res, "Mappings", mappings);
-    }
-  });
-});
+      if (err) {
+        resHelper.error(res, err);
+      } else {
+        resHelper.success(res, "Mappings", mappings);
+      } // else
+    }); // find
+  } // else
+}); // router get
 
 module.exports = router;
