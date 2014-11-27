1a717f10b7b85d2fd3c2f1c207e1004fc07a608f
Harihar Subramanyam <hahasubbu@hotmail.com>
2014-11-27 13:53:38 -0500

    Commented global.js

--- client/source/assets/js/global.js
+++ client/source/assets/js/global.js
@@ -28,16 +28,14 @@ Menyou = {};
     }
   };
 
-  /*location: {
-      lat: 42.358638, // MIT is default location.
-      lon: -71.093345,
-      radius: 1500, // in meters
-      city: 'Cambridge, MA'
-    }*/
   // set current state as default state
   Menyou.state = $.extend(true, {}, Menyou.DEFAULT_STATE);
 
+  // The "token" cookie will contain the authentication token.
   Menyou.COOKIE_NAME = "token";
+
+  // Mappings take a source keyword (ex. "shellfish") and maps it to a set of target keywords
+  // (ex. "oyster", "clam", "lobster", "crab")
   Menyou.Mappings = null; 
 
 })();
