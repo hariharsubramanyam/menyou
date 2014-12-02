e1bc6102e84072923b8655b6610270723169337c
Harihar Subramanyam <hahasubbu@hotmail.com>
2014-12-02 15:48:30 -0500

    README changes

--- client/source/assets/js/tests.js
+++ client/source/assets/js/tests.js
@@ -1,6 +1,8 @@
 // If true, the tests will be run instead of the app. If false, then the app will run as normal.
 Menyou.shouldTest = false;
 
-if (!Menyou.shouldTest) {
-  $("#qunit").remove();
-}
+$(document).ready(function() {
+  if (!Menyou.shouldTest) {
+    $("#qunit").remove();
+  }
+});
